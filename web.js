5a88a10ea53976ef454faf110f2c1b351b5f1642
John Wei <sjwei21@gmail.com>
2013-07-10 02:52:23 +0000

    Fix encoding issue.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   fs.readFileSync('index.html', function(err, data) {
-    var buffer = new Buffer(data.length);
+    var buffer = new Buffer(data);
     response.send(buffer.toString());
   });
 });
