f47c7797c1339db78dd54274aebfb7ed9d4095e4
John Wei <sjwei21@gmail.com>
2013-07-10 03:59:12 +0000

    Moved fs call.

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  fs.readFileSync('index.html', function(err, data) {
+  fs.readFileSync('/index.html', function(err, data) {
     //var buffer = new Buffer(data);
     //response.send(buffer.toString('utf-8'));
     //response.send(data);
